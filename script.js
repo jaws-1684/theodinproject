945c7dc4d76dc1b0d2c66f139a728a311b2eea48
danscorpan <dis.131998@gmail.com>
2024-11-05 16:06:10 +0200

    Write the logic to get the human choice

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
- function getComputerChoice(items) {
+function getComputerChoice(items) {
     return items[Math.floor(Math.random() * items.length)];
     
 }
+const arrayValues = ['rock','paper','scissors'];
 
-const arrayValues = ['rock','paper','scissors']
 
+function getHumanChoice() {
+    let choice = prompt('Enter your choice (Rock, Paper or Scissors): ');
+    return choice;
+}
 
