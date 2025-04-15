d192a05400e86a2066e1af4628ca865c5ee2a44e
dan <dis.131998@gmail.com>
2025-04-15 18:10:31 +0300

    add: Edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+
 	def create
 		 @user = User.new(user_params)
 
