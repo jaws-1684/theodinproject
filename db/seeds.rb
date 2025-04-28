03acd4791ecb5abd9a8f8b1a0241d1d3deb4c597
dan <dis.131998@gmail.com>
2025-04-28 11:22:54 +0300

    Create: Airport and Flight models(&Set up the associations).

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,32 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+airport_objects = {}
+
+airports = [
+  "CDG", "LHR", "AMS", "FRA", "MAD", "BCN", "IST", "MUC", "FCO", "ZRH",
+  "CPH", "ARN", "BRU", "VIE", "DUB", "OSL", "WAW", "PRG", "LIS", "ATH",
+  "HEL", "MXP", "LED", "MAN", "DUS", "SVO", "KRK", "GVA", "LYS", "BUD"
+]
+
+airports.each do |code|
+	airport = Airport.find_or_create_by!(airport_code: code)
+	airport_objects[code] = airport
+end
+
+
+50.times do
+  departure_code = airports.sample
+  arrival_code = (airports - [departure_code]).sample 
+
+  departure_date = Faker::Time.forward(days: 30, period: :day) 
+  flight_duration = rand(90..240) 
+
+  Flight.create!(
+    departure_airport: airport_objects[departure_code],
+    arrival_airport: airport_objects[arrival_code],
+    start_datetime: departure_date,
+    flight_duration: flight_duration
+  )
+end
