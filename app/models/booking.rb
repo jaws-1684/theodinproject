ad8dc87b247269f125d1a43f36dd29eafb8f18ef
dan <dis.131998@gmail.com>
2025-04-30 11:26:22 +0300

    refactor: models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :flight
-  has_many :passengers, dependent: :destroy, inverse_of: :booking
+  has_many :passengers, dependent: :destroy
 
-  accepts_nested_attributes_for :passengers
+  accepts_nested_attributes_for :passengers, reject_if: lambda { |attributes| attributes['name'].blank? || attributes['email'].blank? }
 end
