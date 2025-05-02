b2091954f2264028ef18014b913936dc2a0d18c6
dan <dis.131998@gmail.com>
2025-05-02 18:21:04 +0300

    add: functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+   root "static_pages#index"
+  get '/index', to: 'static_pages#index'
+ 
+
 end
